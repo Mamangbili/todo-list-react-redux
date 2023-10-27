61f61819461f93079f81de37fbb3f70a35902ce1
Habil <legendaryma30@gmail.com>
2023-10-27 13:29:09 +0700

    + Button Component

--- src/App.jsx
+++ src/App.jsx
@@ -3,46 +3,60 @@
 import { useState } from "react";
 
 function App() {
-
   return (
     <>
       <h1>What's the plan for today?</h1>
       <div>
-        <Button/>kkkkk
+        <Button className="bg-red-300"  />
       </div>
-
 
       <div>
         <button></button>
         <button></button>
         <button></button>
       </div>
-      
+
       <ul>
         <li></li>
       </ul>
     </>
-
   );
 }
 
 export default App;
 
 const Input = () => {
-    const [input, setIput] = useState()
-    const inputHandler = (e) => {
-       setIput(e.targer.value) 
-    }
-    return(<>
-        <input onChange={e=>inputHandler(e)} value={input} type="text" placeholder="What to do" />
-    </>)
-}
+  const [input, setIput] = useState();
+  const inputHandler = (e) => {
+    setIput(e.targer.value);
+  };
+  return (
+    <>
+      <input
+        onChange={(e) => inputHandler(e)}
+        value={input}
+        type="text"
+        placeholder="What to do"
+      />
+    </>
+  );
+};
 
-const Button = ({onClick, width="15%", height="15%", clasName}) => {
-    const style = "bg-color-[#6558f5]"   
-    return (<>
-      <button className={style + className} width={width} height={height} onClick={e=>onClick(e)}>Add</button>
-    </>)
-}
-
-const  
+const Button = ({
+  onClick,
+  width = "200px",
+  height = "15%",
+  className = "",
+}) => {
+  const style = "bg-[#6558f5] ";
+  return (
+    <>
+      <button
+        className={style + className}
+        style={{ width:  width , height:  height  }}
+        onClick={(e) => onClick(e)}>
+        Add
+      </button>
+    </>
+  );
+};
